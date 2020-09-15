d9a9c1b59a3b60b4fe1cbfededf9bd9e04317f6e
Dzulfanni <belajarayok866@gmail.com>
2020-09-15 21:20:22 +0700

    first commit

---
+++ es6.js
@@ -0,0 +1,38 @@
+// no1
+const pondokProgrammer = ()=>{
+    return pondokProgrammer
+}
+
+// no2
+const arr = [90, 102, 1231, 3112, 801, 812];
+
+var [a, b, c, d, e, f]= arr;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+
+no3
+const person = { name: 'rizal', asal : 'Prabumulih' };
+
+const {name, asal} = arr;
+
+console.log(name);
+console.log(asal);
+
+no4
+
+const prabu = ["Rizal", "Prabumulih", "SMKprabumulih","prabujaya","sukajadi"];
+const [nama, asal, ...rest]=prabu
+
+
+console.log(nama);
+console.log(asal);
+console.log(...rest);
+
+
+// 
+
+
